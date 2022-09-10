1aaa3214006f17bb5832120925a0594c07d69468
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2022-09-10 15:47:22 +0530

    Tree DS Ques

---
+++ sumOfAllNodes.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <queue>
+#include "TreeNode.h"
+using namespace std;
+
+int sumOfAllNodes(TreeNode<int>* root){
+    int sum=root->data;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        sum+=sumOfAllNodes(root->children[i]);
+    }
+    return sum;
+}
+
+void printTree3(TreeNode<int> *root)
+{
+    // cout<<root->data<<endl;
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (!pendingNodes.empty())
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data <<": ";
+        for (int i = 0; i < front->children.size(); i++)
+        {
+           cout<<front->children[i]->data<<" ";
+           pendingNodes.push(front->children[i]);
+        }
+
+        cout<<"\n";
+        
+    }
+}
+
+TreeNode<int> *inputTree2()
+{
+    cout << "Enter root data\n";
+    int rootData;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+    queue<TreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter Number of children of " << front->data << endl;
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++)
+        {
+            cout << "Enter number " << i + 1 << " child of " << front->data << endl;
+            int childData;
+            cin >> childData;
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            pendingNodes.push(child);
+            front->children.push_back(child);
+        }
+    }
+    return root;
+}
+
+int main(){
+    TreeNode<int>* root = inputTree2();
+    printTree3(root);
+
+   int sum = sumOfAllNodes(root);
+   cout<<"Sum of all nodes : "<<sum;
+}
